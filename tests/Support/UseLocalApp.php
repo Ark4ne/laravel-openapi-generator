3f7e84d3b864829e1a7851af85fd27c4ca892448
Ark4ne <gui.allegret@gmail.com>
2024-08-07 16:08:10 +0200

    fix: phpunit

--- tests/Support/UseLocalApp.php
+++ tests/Support/UseLocalApp.php
@@ -14,11 +14,6 @@ trait UseLocalApp
         ];
     }
 
-    public function useLocalApp()
-    {
-        $this->loadMigrationsFrom(__DIR__ . '/../app/migrations.php');
-    }
-
     protected function defineRoutes($router)
     {
         include __DIR__ . '/../app/routes.php';
