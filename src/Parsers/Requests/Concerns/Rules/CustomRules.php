cbdf62251e9a8a80d5671bf2643759940a28aea7
Ark4ne <gui.allegret@gmail.com>
2024-10-30 15:44:09 +0100

    feat: support laravel new ValidationRule

--- src/Parsers/Requests/Concerns/Rules/CustomRules.php
+++ src/Parsers/Requests/Concerns/Rules/CustomRules.php
@@ -3,10 +3,11 @@
 namespace Ark4ne\OpenApi\Parsers\Requests\Concerns\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Contracts\Validation\ValidationRule;
 
 trait CustomRules
 {
-    public function parseCustomRules(Rule $rule, array $parameters)
+    public function parseCustomRules(ValidationRule | Rule $rule, array $parameters)
     {
         foreach (config('openapi.parsers.rules') as $ruleClass => $parserClass) {
             if ($rule instanceof $ruleClass) {
