afd594288434dc9402b9b389866ce2f4273ad811
Ark4ne <gui.allegret@gmail.com>
2025-03-19 16:32:42 +0100

    fix: digits rule, can't be an integer on openapi

--- src/Parsers/Requests/Concerns/Rules/CommonRules.php
+++ src/Parsers/Requests/Concerns/Rules/CommonRules.php
@@ -257,24 +257,16 @@ trait CommonRules
      */
     public function parseDigits(array $parameters): void
     {
-        $this->parameter->string(false);
+        $this->parameter->string();
 
-        if ($this->parameter->type === Parameter::TYPE_INTEGER || $this->parameter->type === Parameter::TYPE_NUMBER) {
-            if (isset($parameters[0], $parameters[1])) {
-                $this->parameter->min(10 ** ((int)$parameters[0] - 1));
-                $this->parameter->max(10 ** (int)$parameters[1] - 1);
-            } else if (isset($parameters[0])) {
-                $this->parameter->min(10 ** ((int)$parameters[0] - 1));
-                $this->parameter->max(10 ** (int)$parameters[0] - 1);
-            }
-        } else if (isset($parameters[0], $parameters[1])) {
+        if (isset($parameters[0], $parameters[1])) {
             $this->parameter->pattern('digits:/[0-9]+/\{' . $parameters[0] . ', ' . $parameters[1] . '}');
             $this->parameter->min((int)$parameters[0]);
             $this->parameter->max((int)$parameters[1]);
-        }
-        else if (isset($parameters[0])) {
+        } else if (isset($parameters[0])) {
             $this->parameter->pattern('digits:/[0-9]+/\{' . $parameters[0] .'}');
             $this->parameter->min((int)$parameters[0]);
+            $this->parameter->max((int)$parameters[0]);
         }
     }
 
