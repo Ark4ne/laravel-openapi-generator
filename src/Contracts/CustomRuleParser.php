ac02c29f38fd500cc82f5a912bba3d420d80af78
Ark4ne <gui.allegret@gmail.com>
2025-07-02 14:40:46 +0200

    fix: update parse method to accept ValidationRule in addition to Rule

--- src/Contracts/CustomRuleParser.php
+++ src/Contracts/CustomRuleParser.php
@@ -4,16 +4,17 @@ namespace Ark4ne\OpenApi\Contracts;
 
 use Ark4ne\OpenApi\Documentation\Request\Parameter;
 use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Contracts\Validation\ValidationRule;
 
 interface CustomRuleParser
 {
     /**
      * @param \Ark4ne\OpenApi\Documentation\Request\Parameter $parameter
-     * @param \Illuminate\Contracts\Validation\Rule           $rule
+     * @param ValidationRule|Rule                             $rule
      * @param string[]                                        $parameters
      * @param array{rule: string|Rule, parameters:string[]}[] $rules
      *
      * @return void
      */
-    public function parse(Parameter $parameter, Rule $rule, array $parameters, array $rules): void;
+    public function parse(Parameter $parameter, ValidationRule|Rule $rule, array $parameters, array $rules): void;
 }
