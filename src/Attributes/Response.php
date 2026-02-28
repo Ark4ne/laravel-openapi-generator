ddd46a52a14774c3474833da77e9037ba07220e8
Ark4ne <gui.allegret@gmail.com>
2026-02-28 21:15:40 +0100

    feat: update Response attribute to support both class and method targets

--- src/Attributes/Response.php
+++ src/Attributes/Response.php
@@ -5,7 +5,7 @@ namespace Ark4ne\OpenApi\Attributes;
 
 use Attribute;
 
-#[Attribute(Attribute::TARGET_CLASS)]
+#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
 class Response
 {
     /**
